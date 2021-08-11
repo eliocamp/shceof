36686363050b80991cafe8db07047eb916b1a26d
Elio Campitelli <eliocampitelli@gmail.com>
2021-08-11 16:43:22 -0300

    Cambia coord_polar

--- R/maps.R
+++ R/maps.R
@@ -8,40 +8,46 @@
 #' @param ... other arguments passed to [ggplot2::geom_polygon()]
 #'
 #' @export
-geom_qmap <- function(subset = identity, color = "gray50", size = 0.2,
+geom_qmap <- function(subset = identity,
+                      crop = NULL,
+                      color = "gray50", size = 0.2,
                       fill = NA, wrap = c(0, 360), weighting = 0.7,
                       keep = 0.015, ...) {
   lon <- lat <- group <- NULL
   data <- map_simple(wrap = wrap, keep  = keep, weighting = weighting)
 
-  data <- data %>%
-    fortify() %>%
-    data.table::as.data.table() %>%
-    .[, c("long", "lat", "group")] %>%
-    data.table::setnames("long", "lon")
+  if (!is.null(crop)) {
+    bbox <- sf::st_bbox(data)
+
+    for (n in names(crop)) {
+      bbox[[n]] <- crop[[n]]
+    }
+
+    data <- sf::st_crop(data, bbox)
+  }
+
+
   subset <- purrr::as_mapper(subset)
   data <- subset(data)
 
-  ggplot2::geom_polygon(data = data,
-                        ggplot2::aes(lon, lat, group = group),
-                        color = color,
-                        size = size,
-                        fill = fill,
-                        ...)
+  ggplot2::geom_sf(data = data,
+                   inherit.aes = FALSE,
+                   color = color,
+                   size = size,
+                   fill = fill,
+                   ...)
+
 }
 
 map_simple <- function(wrap = c(0, 360), keep = 0.015, weighting = 0.7) {
   map <- maps::map("world", fill = TRUE,
                    col = "transparent", plot = FALSE, wrap = wrap)
-  IDs <- vapply(strsplit(map$names, ":"), function(x) x[1],
-                "")
-  proj <- sp::CRS("+proj=longlat +datum=WGS84")
-  map <- maptools::map2SpatialPolygons(map, IDs = IDs,
-                                       proj4string = proj)
-
+  map <- sf::st_as_sf(map)
   if (keep != 1) {
     map <- rmapshaper::ms_simplify(map, keep = keep, weighting = weighting)
   }
+
+
   map
 }
 
@@ -54,3 +60,9 @@ fortify <- ggplot2::fortify
 }
 
 
+
+`%||%` <- function (x, y) {
+  if (is_null(x))
+    y
+  else x
+}
